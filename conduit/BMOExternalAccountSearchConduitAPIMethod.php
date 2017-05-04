e753474c419c624f2ae80a3688e4187a184dafd4
David Walsh <davidwalsh83@gmail.com>
2017-05-04 12:34:38 -0500

    Add Conduit API method for retrieving a user PHID from a BMO ID

---
+++ conduit/BMOExternalAccountSearchConduitAPIMethod.php
@@ -0,0 +1,40 @@
+<?php
+
+final class BMOExternalAccountSearchConduitAPIMethod
+  extends UserConduitAPIMethod {
+
+  public function getAPIMethodName() {
+    return 'bmoexternalaccount.search';
+  }
+
+  public function getMethodDescription() {
+    return pht('Retrieve external user PHID data based on BMO ID.');
+  }
+
+  public function defineParamTypes() {
+    return array('accountids' => 'required list<string>');
+  }
+
+  protected function defineReturnType() {
+    return 'nonempty dict<string, wild>';
+  }
+
+  protected function execute(ConduitAPIRequest $request) {
+    $account_ids = $request->getValue('accountids');
+
+    $handles = id(new PhabricatorExternalAccountQuery())
+      ->setViewer($request->getUser())
+      ->withAccountIDs($account_ids)
+      ->execute();
+
+    $result = array();
+    foreach ($handles as $user => $handle) {
+      $result[] = array(
+        'id' => $handle->getAccountID(),  // The BMO ID
+        'phid' => $handle->getPHID()      // The Phabricator PHID
+      );
+    }
+
+    return $result;
+  }
+}
