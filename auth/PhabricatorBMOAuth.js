c7533460cde3f49b6e7af0a3389cd06f2874b156
David Walsh <davidwalsh83@gmail.com>
2017-06-21 09:33:52 -0500

    Move admin form check to self-executing function to prevent global leak

--- auth/PhabricatorBMOAuth.js
+++ auth/PhabricatorBMOAuth.js
@@ -2,10 +2,12 @@
  * @provides bmo-auth-js
  */
 
-// Display the admin login form if "?admin"
-if((new URLSearchParams(window.location.search)).has('admin')) {
-  var adminForm = document.querySelector('form[action="/auth/login/password:self/"]');
-  if(adminForm) {
-    adminForm.classList.add('bmo-show');
+(function() {
+  // Display the admin login form if "?admin"
+  if((new URLSearchParams(window.location.search)).has('admin')) {
+    var adminForm = document.querySelector('form[action="/auth/login/password:self/"]');
+    if(adminForm) {
+      adminForm.classList.add('bmo-show');
+    }
   }
-}
+})();
