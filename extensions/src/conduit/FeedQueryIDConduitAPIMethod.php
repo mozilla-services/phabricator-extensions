776872a82db61b737db21ec3b84b93cb757014c3
David Lawrence <dkl@mozilla.com>
2017-11-28 10:54:28 -0500

    conduit api: Create new feed.query_id conduit API method to page using the id column instead of the chronologicalKey value.
    Summary:
    After some thorough discussion, it was decided to that it would be better to use the ID column value for paging transactions in
    the custom API endpoint feed.query_id due to the method of how chronologicalKey (default paging value) is generated.
    chronologicalKey is a combination of the epoch and a random generated value. It was determined that it could be possible
    to have transactions out of order or missed entirely by using it for paging and filtering of transactions. Missing transactions
    when using feed.query could be detrimental to how BMO uses the transaction data to determine revision privacy and other
    action due to revision changes.
    
    Changes:
    
    - Create new feed.query_id Conduit API call to allow paging based in story id instead of chronologicalKey.
    - Patch to Phabricator code to remove final/protected declarations for classes needed for above.
    - Subclassed PhabricatorQueryFeed to make id the default paging column instead of chronologicalKey.
    - Updated phutil_library_map to include new subclassed libraries.
    
    Reviewers: davidwalsh
    
    Reviewed By: davidwalsh
    
    Bug #: 1418004
    
    Differential Revision: https://phabricator.services.mozilla.com/D259

--- extensions/src/conduit/FeedQueryIDConduitAPIMethod.php
+++ extensions/src/conduit/FeedQueryIDConduitAPIMethod.php
@@ -1,71 +1,21 @@
 <?php
 
 /*
- * This file is a modified copy of src/applications/feed/conduit/FeedQueryConduitAPIMethod.php
- * that was needed to give the ability to filter transactions based on epoch timestamp values.
- * This Conduit API method adds the epochStart and epochEnd parameters that can be used for
- * example, to return only transactions that occurred after a specific epoch instead of all
- * transactions. The desire is to push this minor change to upstream Phabricator to be included
- * in the upstream feed.query API method.
+ * This file is a subclass of src/applications/feed/conduit/FeedQueryConduitAPIMethod.php
+ * that was needed to give the ability to filter transactions based on ID values.
  */
 
-final class FeedQueryEpochConduitAPIMethod extends FeedConduitAPIMethod {
+final class FeedQueryIDConduitAPIMethod extends FeedQueryConduitAPIMethod {
 
   public function getAPIMethodName() {
-    return 'feed.query_epoch';
-  }
-
-  public function getMethodStatus() {
-    return self::METHOD_STATUS_UNSTABLE;
-  }
-
-  public function getMethodDescription() {
-    return pht('Query the feed for stories');
+    return 'feed.query_id';
   }
 
   private function getDefaultLimit() {
     return 100;
   }
 
-  protected function defineParamTypes() {
-    return array(
-      'filterPHIDs' => 'optional list <phid>',
-      'limit' => 'optional int (default '.$this->getDefaultLimit().')',
-      'after' => 'optional int',
-      'before' => 'optional int',
-      'view' => 'optional string (data, html, html-summary, text)',
-      'epochStart' => 'optional start epoch int',
-      'epochEnd' => 'optional end epoch int',
-    );
-  }
-
-  private function getSupportedViewTypes() {
-    return array(
-      'html' => pht('Full HTML presentation of story'),
-      'data' => pht('Dictionary with various data of the story'),
-      'html-summary' => pht('Story contains only the title of the story'),
-      'text' => pht('Simple one-line plain text representation of story'),
-    );
-  }
-
-  protected function defineErrorTypes() {
-
-    $view_types = array_keys($this->getSupportedViewTypes());
-    $view_types = implode(', ', $view_types);
-
-    return array(
-      'ERR-UNKNOWN-TYPE' =>
-        pht(
-          'Unsupported view type, possibles are: %s',
-          $view_types),
-    );
-  }
-
-  protected function defineReturnType() {
-    return 'nonempty dict';
-  }
-
-  protected function execute(ConduitAPIRequest $request) {
+  public function execute(ConduitAPIRequest $request) {
     $results = array();
     $user = $request->getUser();
 
