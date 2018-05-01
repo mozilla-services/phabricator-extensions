e9a897b439f0d6b474323a9b9e0ee15eca79c1b1
Piotr Zalewa <piotr@zalewa.info>
2018-05-01 11:16:07 +0200

    secure revisions: Warn about public children
    Test Plan:
    Create a secure revision and at least one public depending on the secure one.
    Check if the warning is displayed.
    
    Reviewers: dkl
    
    Reviewed By: dkl
    
    Subscribers: mcote, zalun, ekr, smacleod
    
    Tags: #secure-revision
    
    Bug #: 1446188
    
    Differential Revision: https://phabricator.services.mozilla.com/D1056

--- extensions/src/differential/view/DifferentialRevisionWarning.php
+++ extensions/src/differential/view/DifferentialRevisionWarning.php
@@ -11,11 +11,73 @@ class DifferentialRevisionWarning extends Phobject {
 
   public function createWarnings($viewer, $revision) {
     $this->warnings[] = $this->createSecurityWarning($viewer, $revision);
+    $this->warnings[] = $this->createPublicDependencyWarning($viewer, $revision);
     return $this->warnings;
   }
 
   public function createSecurityWarning($viewer, $revision) {
-    // Load assigned projects (tags).
+    if (!isRevisionPrivate($viewer, $revision)) {
+      return null;
+    }
+    $warning = new PHUIInfoView();
+    $warning->setTitle(pht('This is a secure revision.'));
+    $warning->setSeverity(PHUIInfoView::SEVERITY_WARNING);
+    $warning->appendChild(hsprintf(pht(
+      'Please use Bugzilla CC list to manage access to this revision.')));
+    return $warning->render();
+  }
+
+  /**
+   * Display a warning for a secure revision which has a public dependency
+   */
+  public function createPublicDependencyWarning($viewer, $revision) {
+    if (!isRevisionPrivate($viewer, $revision)) {
+      return null;
+    }
+
+    // Find depending revisions.
+    $stack_graph = id(new DifferentialRevisionGraph())
+      ->setViewer($viewer)
+      ->setSeedPHID($revision->getPHID())
+      ->setLoadEntireGraph(true)
+      ->loadGraph();
+
+    if ($stack_graph->isEmpty()) {
+      return null;
+    }
+
+    $stack_graph->newGraphTable();
+    $parent_type = DifferentialRevisionDependedOnByRevisionEdgeType::EDGECONST;
+    $reachable = $stack_graph->getReachableObjects($parent_type);
+
+    // Remove depending revisions which are closed or private
+    foreach ($reachable as $key => $reachable_revision) {
+      if ($reachable_revision->isClosed() or isRevisionPrivate($viewer, $reachable_revision)) {
+        unset($reachable[$key]);
+      }
+    }
+
+    if (!$reachable) {
+      return null;
+    }
+
+    $plural = '';
+    if (count($reachable) > 1) {
+      $plural = 's';
+    }
+    $warning = new PHUIInfoView();
+    $warning->setTitle(pht(
+      'A public revision%s is depending on this secure revision.',
+      $plural));
+    $warning->setSeverity(PHUIInfoView::SEVERITY_WARNING);
+    $warning->appendChild(hsprintf(pht(
+      'Please change the child revision%s to private by setting the security flag in Bugzilla.',
+      $plural)));
+    return $warning->render();
+  }
+}
+
+function isRevisionPrivate($viewer, $revision) {
     $revision_projects = PhabricatorEdgeQuery::loadDestinationPHIDs(
       $revision->getPHID(),
       PhabricatorProjectObjectHasProjectEdgeType::EDGECONST);
@@ -31,15 +93,5 @@ class DifferentialRevisionWarning extends Phobject {
       ->withNames(array('secure-revision'))
       ->executeOne();
 
-    if (!in_array($secure_group->getPHID(), $revision_projects)) {
-      return null;
-    }
-
-    $warning = new PHUIInfoView();
-    $warning->setTitle(pht('This is a secure revision.'));
-    $warning->setSeverity(PHUIInfoView::SEVERITY_WARNING);
-    $warning->appendChild(hsprintf(pht(
-      'Please use Bugzilla CC list to manage access to this revision.')));
-    return $warning->render();
-  }
+    return in_array($secure_group->getPHID(), $revision_projects);
 }
