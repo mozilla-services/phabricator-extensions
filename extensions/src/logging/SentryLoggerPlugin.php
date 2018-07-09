33955e58df2c3380849b21c75dc075425497ab5d
David Lawrence <dkl@mozilla.com>
2018-07-09 15:49:39 -0400

    Bug 1463971 - phabricator session cookies are leaked to mozilla's sentry instance
    Summary:
    The Sentry logger plugin should omit or overwrite sensitive data before sending to
    the Sentry server. Types of fields include cookies, API tokens, csrf tokens, and
    special headers.
    
    Test Plan:
    1. Apply patch and include phabricator-extensions directory into demo docker environment (docker-compose.phabricator.yml)
    2. Find Sentry DSN for mozphab-dev project
    3. Update sentry.dsn as admin in Phabricator configuration.
    4. Generate an Exception that will be sent to Phabricator which at the moment is most exceptions.
    5. Observe in the Sentry dashboard that the sensitive data is filtered.
    
    Reviewers: glob, zalun
    
    Reviewed By: glob
    
    Subscribers: mcote, dkl, ulfr, zalun, glob, smacleod, ckolos, claudijd
    
    Tags: #secure-revision, #bmo-conduit-security
    
    Bug #: 1463971
    
    Differential Revision: https://phabricator.services.mozilla.com/D1917

--- extensions/src/logging/SentryLoggerPlugin.php
+++ extensions/src/logging/SentryLoggerPlugin.php
@@ -8,6 +8,50 @@ class SentryLoggerPlugin extends Phobject {
       array(__CLASS__, 'handleError'));
   }
 
+  public static function parse_query_str($str) {
+    # result array
+    $arr = array();
+
+    # split on outer delimiter
+    $pairs = explode('&', $str);
+
+    # loop through each pair
+    foreach ($pairs as $i) {
+      # split into name and value
+      list($name,$value) = explode('=', $i, 2);
+
+      # if name already exists
+      if( isset($arr[$name]) ) {
+        # stick multiple values into an array
+        if( is_array($arr[$name]) ) {
+          $arr[$name][] = $value;
+        }
+        else {
+          $arr[$name] = array($arr[$name], $value);
+        }
+      }
+      # otherwise, simply stick it in a scalar
+      else {
+        $arr[$name] = $value;
+      }
+    }
+
+    # return result array
+    return $arr;
+  }
+
+  public static function generate_query_str($array) {
+    $params = array();
+    foreach ($array as $k => $v) {
+      if (is_array($v))
+        $params[] = append_params($v, urlencode($k));
+      else
+        $params[] = urlencode($k) . '=' . urlencode($v);
+    }
+
+    return implode('&', $params);
+  }
+
   public static function handleError($event, $value, $metadata) {
     $sentry_dsn = PhabricatorEnv::getEnvConfigIfExists('sentry.dsn');
 
@@ -19,7 +63,39 @@ class SentryLoggerPlugin extends Phobject {
     $root = phutil_get_library_root('phabricator');
     $root = dirname($root);
     require_once $root . '/externals/extensions/autoload.php';
-    $client = new Raven_Client($sentry_dsn);
+    $client = new Raven_Client($sentry_dsn, array(
+      'processors' => array(
+        'Raven_Processor_SanitizeHttpHeadersProcessor', // Sanitize the HTTP headers
+      ),
+      'processorOptions' => array(
+        'Raven_Processor_SanitizeHttpHeadersProcessor' => array(
+          'sanitize_http_headers' => array('Cookie', 'X-Phabricator-Csfr')
+        )
+      ),
+      'send_callback' => function(&$data) {
+        // Sanitize HTTP POST data
+        $fields_re = '/^(__csrf__|token)$/i';
+        $sanitize = function(&$item, $key, $fields_re) {
+          if (empty($key)) {
+            return;
+          }
+          if (preg_match($fields_re, $key)) {
+            $item = '********';
+          }
+        };
+        array_walk_recursive($data['request']['data'], $sanitize, $fields_re);
+
+        // Sanitize query string
+        $query_data = self::parse_query_str($data['request']['query_string']);
+        array_walk_recursive($query_data, $sanitize, $fields_re);
+        $data['request']['query_string'] = self::generate_query_str($query_data);
+
+        // Sanitize cookie data
+        if (isset($data['request']['cookies'])) {
+          $data['request']['cookies']['phsid'] = '********';
+        }
+      }
+    ));
 
     switch ($event) {
       case PhutilErrorHandler::EXCEPTION:
