a7d33414a79c29c287c1547d6236b9fc824f2888
Mitchell Hentges <mhentges@mozilla.com>
2019-12-09 15:41:41 -0800

    phabricator-extensions: No bug - make update_build_url py3-compatible
    update_build_url.py used py2-style print statements, this change should ensure this file works with both py2 and py3
    
    Differential Revision: https://phabricator.services.mozilla.com/D56482

--- update_build_url.py
+++ update_build_url.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.7
 """ Update the version,json to contain phabext information"""
+from __future__ import print_function
 import json
 import sys
 
@@ -7,13 +8,13 @@ try:
     with open('/app/version.json', 'r') as f:
         mozphab_circle_data = json.load(f)
 except IOError:
-    print "version.json not found"
+    print("version.json not found")
     mozphab_circle_data = {}
 try:
     with open('/app/phabext.json', 'r') as g:
         phabext_circle_data = json.load(g)
 except IOError:
-    print "phabext.json not found"
+    print("phabext.json not found")
     phabext_circle_data = {}
 mozphab_circle_data.update(phabext_circle_data)
 
@@ -23,5 +24,5 @@ try:
         json.dump(mozphab_circle_data, f)
         f.close()
 except IOError:
-    print "Could not create version.json"
+    print("Could not create version.json")
     sys.exit()
