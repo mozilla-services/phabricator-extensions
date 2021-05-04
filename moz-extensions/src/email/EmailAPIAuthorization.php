e0120fc9541ea6fedba21ffab4839d8709a4fee3
Mitchell Hentges <mhentges@mozilla.com>
2021-05-04 15:47:58 -0400

    Bug 1632996: Use security group to authorize email endpoint
    To improve debuggability in dev, qa, and prod, we allow users
    other than "email-bot" to access the API.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114276

--- moz-extensions/src/email/EmailAPIAuthorization.php
+++ moz-extensions/src/email/EmailAPIAuthorization.php
@@ -7,16 +7,22 @@ class EmailAPIAuthorization {
    * @throws ConduitException
    */
   public static function assert(PhabricatorUser $user) {
+    $emailProject = (new PhabricatorProjectQuery())
+      ->setViewer(PhabricatorUser::getOmnipotentUser())
+      ->needMembers(true)
+      ->withNames(["mozilla-phabricator-emails"])
+      ->executeOne();
+
+    if ($emailProject && in_array($user->getPHID(), $emailProject->getMemberPHIDs())) {
+      return;
+    }
+
     if (PhabricatorEnv::getEnvConfig('bugzilla.url') == "http://bmo.test") {
       // When running in the local development environment, allow using the API as any user
       return;
     }
 
-    if ($user->getUserName() == 'email-bot' && $user->getIsSystemAgent() && $user->getIsApproved()) {
-      return;
-    }
-
     throw (new ConduitException('ERR-INVALID-AUTH'))
-      ->setErrorDescription('Only the "email-bot" user can use this endpoint');
+      ->setErrorDescription('Must be part of the "mozilla-phabricator-emails" security group');
   }
 }
